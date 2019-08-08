9c8bf77b54445b4f4f757ed448ee56325d172219
misterhaan <misterhaan@users.noreply.github.com>
2019-08-08 16:19:57 -0500

    fileoperation assembly

---
+++ IO.Files.FileOperation/ComDisposer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace au.IO.Files.FileOperation {
+	/// <summary>
+	/// IDisposable wrapper for COM objects.
+	/// </summary>
+	/// <typeparam name="T">Type of COM object.</typeparam>
+	internal class ComDisposer<T> : IDisposable where T : class {
+		/// <summary>
+		/// Initialize a COM disposer for the specified object.
+		/// </summary>
+		/// <param name="obj">COM object that needs to be released.</param>
+		internal ComDisposer(T obj) {
+			if(obj != null && !obj.GetType().IsCOMObject)
+				throw new ArgumentOutOfRangeException("Object must be a COM object.", nameof(obj));
+			Value = obj;
+		}
+
+		/// <summary>
+		/// The wrapped COM object.
+		/// </summary>
+		internal T Value { get; private set; }
+
+		/// <summary>
+		/// Release the COM object if it hasn't already been released.
+		/// </summary>
+		public void Dispose() {
+			if(Value != null) {
+				Marshal.FinalReleaseComObject(Value);
+				Value = null;
+			}
+		}
+	}
+}
---
+++ IO.Files.FileOperation/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("au.IO.Files.FileOperation")]
+[assembly: AssemblyDescription("Provides access to the Windows Explorer file move / copy dialog.  Requires Windows Vista or newer.")]
