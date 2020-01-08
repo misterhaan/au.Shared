ae13fcfed05c8aabbf4500aa86fe49382a3ff0b1
misterhaan <misterhaan@track7.org>
2020-01-07 21:54:14 -0600

    use common assembly info

--- UI/DirectoryBox/Tests/Properties/AssemblyInfo.cs
+++ UI/DirectoryBox/Tests/Properties/AssemblyInfo.cs
@@ -1,20 +1,4 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("11890fde-fffd-497c-bcc4-da2b40db7027")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyTitle("au.UI.DirectoryBox.Tests")]
+[assembly: AssemblyDescription("Tests for the analog underground DirectoryBox control")]
